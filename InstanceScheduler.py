e10efe47f9c8243e467ebd21f45cb68a0f04f73d
Patrick Kuny <31052319+pmkuny@users.noreply.github.com>
2021-07-30 09:22:15 -0500

    Fix multi-line comments

--- InstanceScheduler.py
+++ InstanceScheduler.py
@@ -4,25 +4,24 @@ import json
 import boto3
 import boto3.session
 
-'''
-We use CloudWatch Event Bridge to control the times that fire off a stop_instances() and start_instances()
 
-'''
-Session: Creates a new boto3 session based on the session configuration
-requirements.
+# We use CloudWatch Event Bridge to control the times that fire off a stop_instances() and start_instances()
 
-elligible_instances_tag: Dictionary for defining what tag this Lambda can
-operate on. NOTE that by default and IAM policy, this is currently limited to
-"scheduler": "true"
-'''
+
+# Session: Creates a new boto3 session based on the session configuration
+# requirements.
+
+# elligible_instances_tag: Dictionary for defining what tag this Lambda can
+# operate on. NOTE that by default and IAM policy, this is currently limited to
+# "scheduler": "true"
+
 
 session = boto3.session.Session(region_name='us-west-2')
 eligible_instances_tag = {"scheduler": "true"}
 
-'''
-We need to retrieve a list of the instances with this specific tag applied, by ID.
-CLI: aws ec2 describe-instances --filters Name=tag:scheduler,Values='true' --query Reservations[*].Instances[*].InstanceId --output text
-'''
+# We need to retrieve a list of the instances with this specific tag applied, by ID.
+# CLI: aws ec2 describe-instances --filters Name=tag:scheduler,Values='true' --query Reservations[*].Instances[*].InstanceId --output text
+
 
 def get_instance_ids():
     instance_ids = []
